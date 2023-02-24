f6cc8965a89b3de2ffbcd401e45bfed72e41a08e
jimaandro <jimaandron@gmail.com>
2023-02-24 13:45:01 +0200

    Added comments

--- tests/sourcesRoute.test.js
+++ tests/sourcesRoute.test.js
@@ -1,44 +1,46 @@
+// Disable eslint unresolved import rules
 /* eslint-disable import/no-unresolved */
+
+// Load environment variables from .env file
 require('dotenv').config();
+
+// Import required dependencies and modules
 const {mongoose} = require('../src/config');
 const {http,test,got,listen,app,User} = require('../src/RouteIn');
 const {jwtSign} = require('../src/utilities/authentication/helpers');
 const Source = require('../src/models/source');
 const sinon = require('sinon');
 
-
 // Runs before each test
 test.before(async (t) => {
-  // Create server and prefixUrl
-  t.context.server = http.createServer(app);
-  t.context.prefixUrl = await listen(t.context.server);
-  
-  // Set up HTTP client with prefixUrl
-  t.context.got = got.extend({http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl});
-  
-  // Create a new user for testing
-  user = await User.create({
-    username: 'user',
-    password: 'password',
-    email: 'email',
-  });
-});
+// Create server and prefixUrl
+t.context.server = http.createServer(app);
+t.context.prefixUrl = await listen(t.context.server);
 
+// Set up HTTP client with prefixUrl
+t.context.got = got.extend({http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl});
+
+// Create a new user for testing
+user = await User.create({
+username: 'user',
+password: 'password',
+email: 'email',
+});
+});
 
 // Runs after each test
 test.after.always((t) => {
-  // Close the server
-  t.context.server.close();
-  
-  // Delete the test user
-  User.findByIdAndDelete(user._id);
+// Close the server
+t.context.server.close();
 
-  // Delete any test sources created during testing
-  Source.deleteMany({}, 
-    function(err){
-      if(err) console.log(err);
-      console.log("Successful delete");
-    });
+// Delete the test user
+User.findByIdAndDelete(user._id);
+
+// Delete any test sources created during testing
+Source.deleteMany({}, function(err){
+if(err) console.log(err);
+console.log("Successful delete");
+});
 });
 
 // Test to verify that GET request to /sources with valid authentication returns a list of sources and a 200 status code
@@ -90,49 +92,91 @@ test('GET /sources returns correct response and status code for authenticated us
   
 // Test to verify that POST request to /create-source with invalid data returns a 404 Bad Request status code
 test('POST /create-source with invalid data returns 404 status code', async (t) => {
+    // Initialize the mongoose database
     mongoose();
+    
+    // Create a JSON Web Token with the user's ID
     const token = jwtSign({id: user._id});
-    const invalidSourceData = {name: '', type: 'invalid', url: 'invalid', login: '', passcode: '', vhost: '', owner: user._id, createdAt:''};
-    const {statusCode, body} = await t.context.got.post(`sources/create-source?token=${token}`, {json: invalidSourceData});
-  
+    
+    // Create invalid data for the new source
+    const invalidSourceData = {
+    name: '',
+    type: 'invalid',
+    url: 'invalid',
+    login: '',
+    passcode: '',
+    vhost: '',
+    owner: user._id,
+    createdAt: ''
+    };
+    
+    // Send a POST request to create a new source with the invalid data
+    const {statusCode, body} = await t.context.got.post(sources/create-source?token=${token}, {json: invalidSourceData});
+    
+    // Assert that the response status code is 404
     t.is(statusCode, 404);
-  });
-  
-  // Test to verify that GET request to /sources returns correct response and status code for unauthenticated user
-  test('GET /sources returns correct response and status code for unauthenticated user', async (t) => {
+    });
+    
+    // Test to verify that GET request to /sources returns correct response and status code for unauthenticated user
+    test('GET /sources returns correct response and status code for unauthenticated user', async (t) => {
+    // Initialize the mongoose database
     mongoose();
+    
+    // Send a GET request to get all sources without an authentication token
     const {body} = await t.context.got('sources/sources');
-  
+    
+    // Assert that the response status is 403 and the error message is 'Authorization Error: token missing.'
     t.is(body.status, 403);
     t.is(body.message, 'Authorization Error: token missing.');
-  });
-  
-  // Test to verify that GET request to /sources/:id returns correct response and status code for authenticated user
-  test('GET /sources/:id returns correct response and status code for authenticated user', async (t) => {
+    });
+    
+    // Test to verify that GET request to /sources/:id returns correct response and status code for authenticated user
+    test('GET /sources/:id returns correct response and status code for authenticated user', async (t) => {
+    // Initialize the mongoose database
     mongoose();
+    
+    // Create a JSON Web Token with the user's ID
     const token = jwtSign({id: user._id});
-  
-    const newSource = await Source({name: 'sourceOne', type: '', url: '', login: '', passcode: '', vhost: '', owner: user._id, createdAt: ''}).save();
-  
-    const {body, statusCode} = await t.context.got(`sources/sources?token=${token}`);
-  
+    
+    // Create a new source and save it to the database
+    const newSource = await Source({
+    name: 'sourceOne',
+    type: '',
+    url: '',
+    login: '',
+    passcode: '',
+    vhost: '',
+    owner: user._id,
+    createdAt: ''
+    }).save();
+    
+    // Send a GET request to get all sources with the authentication token
+    const {body, statusCode} = await t.context.got(sources/sources?token=${token});
+    
+    // Assert that the response status is 200 and the success property is true
     t.is(statusCode, 200);
     t.assert(body.success);
-  });
-  
-  
-test('POST /create-source returns correct response and status code when trying to create a new Newsource with a name that already exists ', async (t) => {
-  mongoose();
-  const token = jwtSign({id: user._id});
-  
-  Newsource = await Source({name:'sourceNew',type: '',url:'',login:'',passcode:'',vhost: '',owner: user._id,createdAt:'',
-  }).save();
+    });
 
-  const duplName = 'sourceNew' ; 
-  const sourceDupl={name:duplName} ;
- 
-  const {body} = await t.context.got.post(`sources/create-source?token=${token}`,{json:sourceDupl});
- 
-  t.is(body.status,409);
-  t.is(body.message,'A source with that name already exists.');
+// Test to verify that POST request to /create-source returns the correct response and status code when trying to create a new source with a name that already exists
+test('POST /create-source returns correct response and status code when trying to create a new Newsource with a name that already exists ', async (t) => {
+// Initialize mongoose
+mongoose();
+
+// Generate JWT token
+const token = jwtSign({id: user._id});
+
+// Create a new source with name 'sourceNew'
+Newsource = await Source({name:'sourceNew',type: '',url:'',login:'',passcode:'',vhost: '',owner: user._id,createdAt:''}).save();
+
+// Prepare duplicate source name
+const duplName = 'sourceNew' ;
+const sourceDupl={name:duplName} ;
+
+// Send a POST request to create a new source with duplicate name using the JWT token
+const {body} = await t.context.got.post(sources/create-source?token=${token},{json:sourceDupl});
+
+// Assert that the status code and message returned from the API are correct
+t.is(body.status,409);
+t.is(body.message,'A source with that name already exists.');
 });
